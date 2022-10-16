bce13ad311b384371547a45d1ec8bc22d2d2fa61
Kuro Cybria <rizqikurniawan1863@gmail.com>
2022-10-16 22:39:26 +0700

    update code

--- GrayScale2Binary/ManualG2B.py
+++ GrayScale2Binary/ManualG2B.py
@@ -1,23 +1,22 @@
-from math import degrees
 import cv2 as cv
 import numpy as np
 
 sample = cv.imread('./sample.jpg')
 img = cv.resize(sample, (852, 480), interpolation=cv.INTER_AREA)
 
-color_treeshold = 150
-
 row_len = len(img)
 col_len = len(img[0])
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+treshold = 150
 
 img_bin = gray
 
 for row in range(row_len):
     for col in range(col_len):
         pixel = gray[row, col]
-        if pixel > color_treeshold:
+        if pixel > treshold:
             img_bin[row, col] = 255
         else:
             img_bin[row, col] = 0
